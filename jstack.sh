1dbda71bb777900b9418daac474b87b8c52286ea
erezbashan <erezbashan@carpoolmatchingdispatcher-bundle-beta-row-v999-g475.waze>
2020-05-15 10:58:21 +0000

    .

--- jstack.sh
+++ jstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #set -x
 
-pid=$(ps -ef | grep 'java -X' | grep -v grep | awk '{print \$2}')
-sudo -u linqmap sh -c "jstack $(pid)"
+pid=$(ps -ef | grep 'java -X' | grep -v grep | awk '{print $2}')
+sudo -u linqmap sh -c "jstack ${pid}"
